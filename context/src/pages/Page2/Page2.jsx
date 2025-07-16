01c17e4861e7c0898188637e7f01f7fc60701d00
Keila Prado <keilaprado.pv@gmail.com>
2025-07-16 12:44:39 -0300

    add title color context

--- context/src/pages/Page2/Page2.jsx
+++ context/src/pages/Page2/Page2.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { useTitleColorContext } from "../../hooks/useTitleColorContext"
 
 const Page2 = () => {
+  const { color, dispatch } = useTitleColorContext();
+
+  const setTitleColor = (color) => {
+    dispatch({type: color})
+  }
+
   return (
-    <h2>Página Dois</h2>
+    <>
+      <h2 style={{color: color}}>Página Dois</h2>
+      <div>
+        <button 
+          onClick={() => setTitleColor('RED')}
+          style={{color: 'white', backgroundColor: 'red'}}>
+            Red
+        </button>
+        <button 
+          onClick={() => setTitleColor('GREEN')}
+          style={{color: 'white', backgroundColor: 'green'}}>
+            Green
+        </button>
+        <button 
+          onClick={() => setTitleColor('BLUE')}
+          style={{color: 'white', backgroundColor: 'blue'}}>
+            Blue
+        </button>
+      </div>
+    </>
   )
 }
 
